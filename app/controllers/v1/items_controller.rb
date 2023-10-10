36122ce4fc146cf87452445de88acd75635e9f14
Nilay <nilayanand@gmail.com>
2023-10-10 18:12:27 +0530

    update api for item except quantity param

--- app/controllers/v1/items_controller.rb
+++ app/controllers/v1/items_controller.rb
@@ -1,6 +1,15 @@
 class V1::ItemsController < V1::BaseController
   include BaseCrud
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params.except(:quantity))
+      render json: {message: "Successfully updated", item: @item}
+    else
+      render json: {message: @item.errors}
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(
@@ -11,6 +20,7 @@ class V1::ItemsController < V1::BaseController
         :alert_quantity
     )
   end
+
 
   def items_scope
     Item.all
