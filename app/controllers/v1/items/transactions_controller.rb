58f2073409363f824987fd90b5a305541ba8c726
nilay <nilayanand@gmail.com>
2023-10-18 12:47:47 +0530

    Fix invalid transaction type

--- app/controllers/v1/items/transactions_controller.rb
+++ app/controllers/v1/items/transactions_controller.rb
@@ -2,18 +2,22 @@ class V1::Items::TransactionsController < V1::Items::BaseController
   include BaseCrud
 
   def create
-    @previous_quantity = @item.quantity
-    if params[:transaction_type]
-      if params[:transaction_type] == "received"
-        @new_quantity = @previous_quantity + params[:quantity]
-      elsif params[:transaction_type] == "issued"
-        @new_quantity = @previous_quantity - params[:quantity]
+    if ["received", "issued"].include?(params[:transaction_type])
+      @previous_quantity = @item.quantity
+      if params[:transaction_type]
+        if params[:transaction_type] == "received"
+          @new_quantity = @previous_quantity + params[:quantity]
+        elsif params[:transaction_type] == "issued"
+          @new_quantity = @previous_quantity - params[:quantity]
+        end
+        @item.update!(quantity: @new_quantity)
+        @item.create_transaction(params[:quantity], current_user)
+        render json: { message: 'Successfully updated the Quantity', transaction: @item.item_transactions.as_api_response(:index)}, status: 200
+      else
+        render json: { message: 'Error while updating Quantity' }, status: 402
       end
-      @item.update!(quantity: @new_quantity)
-      @item.create_transaction(params[:quantity], current_user)
-      render json: { message: 'Successfully updated the Quantity', transaction: @item.item_transactions.as_api_response(:index)}, status: 200
     else
-      render json: { message: 'Error while updating Quantity' }, status: 402
+      render json: { message: 'invalid transaction type' }, status: 402
     end
   end
 
